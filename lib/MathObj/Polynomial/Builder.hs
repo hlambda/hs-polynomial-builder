13482ba92d48a8b46337e64acf5b6e793332042b
James Halliday <substack@gmail.com>
2010-02-27 02:42:55 -0900

    cleaned up instances, removing the need for expansion

--- lib/MathObj/Polynomial/Builder.hs
+++ lib/MathObj/Polynomial/Builder.hs
@@ -1,14 +1,19 @@
 module MathObj.Polynomial.Builder (
     Expression(..), Polynomial(..),
-    subs, expand, visit, inTermsOf
+    -- subs, visit, inTermsOf
 ) where
 
-import MathObj.Polynomial hiding (coeffs)
+import MathObj.Polynomial hiding (coeffs,const,negate)
 import Data.Ord (comparing)
 import Data.List (find,intersperse,(\\),sort,groupBy,sortBy)
 import Data.Maybe (fromJust,isJust)
 import Control.Arrow (first,second,(***))
 import Control.Monad (join)
+
+import Data.Traversable
+import Data.Foldable hiding (concatMap,sum,find,product,maximum,concat)
+import Data.Monoid (mappend,mempty,mconcat,appEndo)
+import Prelude hiding (foldl,foldl1)
 
 data Expression a
     = Const a
@@ -22,70 +27,56 @@ data Expression a
     | Var String -- ^ Free-form variable name
     deriving (Eq,Ord)
 
-subexp :: Expression a -> [Expression a]
-subexp (Add xs) = xs
-subexp (Mul xs) = xs
-subexp (Negate x) = [x]
-subexp (Exp x _) = [x]
-subexp _ = []
-
 instance Num a => Show (Expression a) where
     show (Const x) = show x
     show (Add xs) = "(" ++ (concat $ intersperse " + " $ map show xs) ++ ")"
-    show (Mul xs) = "(" ++ (concat $ intersperse " * " $ map show xs) ++ ")"
-    show (Negate x) = "(-(" ++ show x ++ "))"
+    show (Mul xs) = concat $ intersperse " * " $ map show xs
+    show (Negate x) = "-" ++ show x
     show (Exp x n) = "(" ++ show x ++ ")" ++ "^" ++ show n
     show (Var x) = x
     show letter = (:[]) $ fst $ fromJust $ find ((== letter) . snd)
         $ zip ['A'..'Z'] [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
 
-isConst :: Expression a -> Bool
-isConst Const{} = True
-isConst _ = False
-
 instance Num a => Num (Expression a) where
     (Const 0) + x = x
     x + (Const 0) = x
     (Const x) + (Const y) = Const (x + y)
-    (Add xs) + (Add ys) = case (c,filter (not . isConst) zs) of
-        (c,[]) -> c
-        (Const 0,[x]) -> x
-        (c,[x])-> c + x
-        (Const 0,fs) -> Add fs
-        (c,fs) -> Add $ c : fs
-        where
-            c = sum $ filter isConst zs
-            zs = concatMap g $ xs ++ ys
-            g (Add xs) = concatMap g xs
-            g x = [x]
-    x@Add{} + y = x + Add [y]
-    x + y@Add{} = y + Add [x]
+    (Add xs) + (Add ys) = sum $ xs ++ ys
+    (Add xs) + x = Add (x:xs)
+    x + (Add xs) = Add (x:xs)
     x + y = Add [x,y]
     
+    -- constant multiplication
+    (Const x) * (Const y) = Const (x * y)
+    (Const 0) * x = Const 0
+    x * (Const 0) = Const 0
     (Const 1) * x = x
     x * (Const 1) = x
-    (Const x) * (Const y) = Const (x * y)
-    (Mul xs) * (Mul ys) = case (c,filter (not . isConst) zs) of
-        (c,[]) -> c
-        (Const 0,[x]) -> x
-        (c,[x])-> c * x
-        (Const 0,fs) -> Mul fs
-        (c,fs) -> Mul $ c : fs
-        where
-            c = product $ filter isConst zs
-            zs = concatMap g $ xs ++ ys
-            g (Mul xs) = concatMap g xs
-            g x = [x]
-    x@Mul{} * y = x * Mul [y]
-    x * y@Mul{} = y * Mul [x]
+    -- expand polynomials in multiplication
+    x * (Add xs) = sum $ map (x *) xs
+    (Add xs) * x = sum $ map (x *) xs
+    -- build recursive product types
+    (Mul xs) * (Mul ys) = product $ xs ++ ys
+    (Mul xs) * x = Mul (x:xs)
+    x * (Mul xs) = Mul (x:xs)
     x * y = Mul [x,y]
     
+    -- constant subtraction
     (Const x) - (Const y) = Const (x - y)
+    x - y | x == y = Const 0 -- identity zero
+    -- distribute negation over addition
+    x - (Add xs) = sum $ x : map negate xs
+    x - (Negate y) = x + y
     x - y = Add [x,Negate y]
     
-    negate = Negate
-    abs = undefined
-    signum = undefined
+    -- the other stuff
+    negate (Const x) = Const (-x)
+    negate x = Negate x
+    
+    abs (Const x) = Const (abs x)
+    abs x = undefined -- not allowed for now
+    
+    signum = undefined -- not important enough to implement
     fromInteger = Const . fromInteger
 
 instance Fractional a => Fractional (Expression a) where
@@ -96,35 +87,20 @@ data Polynomial a = Univariate [a] | Multivariate [(Expression a, [a])]
 
 -- | Build a polynomial out of an expression
 build :: (Num a, Ord a) => Expression a -> Expression a -> Polynomial a
-build var (Add as) = Univariate $ map f [ 0 .. maxDeg ] where
-    f x = case lookup x terms' of
-        Nothing -> 0
-        Just x -> x
-    terms' = map (first head . second sum . unzip)
-        $ groupBy (\x y -> fst x == fst y) $ sort terms
-    maxDeg = maximum $ map fst terms
-    terms = [ (degree x, coeff x) | x <- as ]
-    
-    coeff (Const x) = 0
-    coeff (Mul ms) = product $ map (\(Const x) -> x) $ filter isConst ms
-    coeff _ = 0
-    
-    degree (Mul ms) = length $ filter (== var) ms
-    degree x | x == var = 1
-    degree _ = 0
+build var (Add xs) = 
 
 -- | Substitute a sub-expression with a replacement in some expression.
 subs :: (Eq a, Ord a) =>
     Expression a -> Expression a -> Expression a -> Expression a
 subs f r = visit g where
-    sf = subexp f
+    sf = subterms f
     g e | e == f = r -- term matches
     g e | (not $ null sf) && (null $ sf \\ se) =
         case e of -- all subterms match
             Add xs -> Add $ r : (se \\ sf)
             Mul xs -> Mul $ r : (se \\ sf)
             x -> x -- just a scalar, will be picked up on different visit
-        where se = subexp e
+        where se = subterms e
     g e = e
 
 -- | Visit all the nodes of an expression, including the root, depth-first.
@@ -137,6 +113,20 @@ visit f expr = visit' f $ f expr where
     visit' f (Exp x n) = f $ Exp (visit' f x) n
     visit' f x = f x
 
+terms :: Expression a -> [Expression a]
+terms x = x : (concatMap terms $ subterms x)
+
+leaves :: Expression a -> [Expression a]
+leaves = filter ((== 0) . length . subterms) . terms
+
+subterms :: Expression a -> [Expression a]
+subterms (Add xs) = xs
+subterms (Mul xs) = xs
+subterms (Negate x) = [x]
+subterms (Exp x _) = [x]
+subterms _ = []
+
+{-
 inTermsOf :: Expression a -> Expression a -> Expression a
 inTermsOf var expr = undefined
 
@@ -150,20 +140,4 @@ classify var expr = sortBy (comparing snd)
 
 sift :: (a -> Bool) -> [a] -> ([a],[a])
 sift f xs = (filter f xs, filter (not . f) xs)
-
-expand :: Num a => Expression a -> Expression a
-expand = visit f where
-    f (Add [x]) = x
-    f (Mul [x]) = x
-    f (Mul xs) = foldl1 g xs
-    --f (Mul xs) = product xs
-    f x = x
-    
-    g x (Add xs) = Add $ concatMap (h . g x) xs
-    g (Add xs) x = Add $ concatMap (h . g x) xs
-    g x (Mul xs) = Mul (x:xs)
-    g (Mul xs) x = Mul (x:xs)
-    g x y = x * y
-    
-    h (Add xs) = xs
-    h x = [x]
+-}
